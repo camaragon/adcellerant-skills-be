87deec0a0e324d911b80aa110cd09e326a157c47
Cameron Aragon <caragon4695@gmail.com>
2021-06-19 11:57:56 -0600

    Setup table for migrations

--- migrations/20210619174309_adcellerant_table.js
+++ migrations/20210619174309_adcellerant_table.js
@@ -1,8 +1,15 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('data', function(table) {
+      table.increments();
+      table.string('product').notNullable();
+      table.string('date').notNullable();
+      table.string('platform').notNullable();
+      table.integer('impressions').notNullable();
+      table.integer('clicks').notNullable();
+  })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable('data')
 };
